f736a65cb94a34f28a5f4b9a86f6160da8a79e90
Bijin <bijindas777@gmail.com>
2022-07-29 12:24:11 +0530

    update

--- routers/products.js
+++ routers/products.js
@@ -1,5 +1,6 @@
 const {Product} = require('../models/product');
 const express = require('express');
+const { Category } = require('../models/category');
 const router = express.Router();
 
 router.get(`/`, async (req, res) =>{
@@ -11,21 +12,60 @@ router.get(`/`, async (req, res) =>{
     res.send(productList);
 })
 
-router.post(`/`, (req, res) =>{
+router.post(`/`, async (req, res) =>{
+     
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('invalid Category')
+
+
     const product = new Product({
         name: req.body.name,
+        description: req.body.description,
+        richDescription:req.body.richDescription,
         image: req.body.image,
-        countInStock: req.body.countInStock
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating:req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured:req.body.isFeatured,
     })
+    ///product =
+    await product.save();
 
-    product.save().then((createdProduct=> {
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
+   if(!product)
+   return res.status(500).send('The product cannot be created')
+   res.send(product);
 })
+
+router.put('/:id',async(req,res)=>{
+
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('invalid Category')
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription:req.body.richDescription,
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating:req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured:req.body.isFeatured,
+        },
+        { new: true}
+    )
+    if(!category)
+    return res.status(500).send('the product cannot be updated !!')
+
+    res.send(product);
+})
+
 
 module.exports =router;
